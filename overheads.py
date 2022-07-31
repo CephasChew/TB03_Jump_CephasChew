0a3f3c732d77d69af6a5750c3a7443b7adeadb0d
Jeff <S10247657@connect.np.edu.sg>
2022-07-31 18:28:21 +0800

    save

--- overheads.py
+++ overheads.py
@@ -1,2 +1,22 @@
+from pathlib import Path
 import csv
+
+file_path = Path.cwd()/"CSV_reports"/"Overheads.py"
+
+with file_path.open(mode="w",encoding='UTF-8',newline ="") as file:
+
+    for file in file_path("Overheads.csv"):
+        emptylist=[]
+with file.open(mode="r", encoding = "UTF-8") as file:
+    text = file.read()
+
+    
+
+
+
+
+
+            
+
+
 
