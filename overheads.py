f65b9a03a1ffec91accd74441671d7529cd37efa
Jeff <S10247657@connect.np.edu.sg>
2022-08-01 15:13:04 +0800

    edit

--- overheads.py
+++ overheads.py
@@ -5,17 +5,29 @@ from platform import java_ver
 import re
 from unicodedata import category
 
-emptylist=[]
+file_path = Path.cwd()/"CSV_reports"
+overheads = Path.cwd()/"CSV_reports"/"Overheads.csv"
+overheads.touch
 
-file_path = Path.cwd()/"CSV_reports"/"Overheads.csv"
+with file_path.open(mode="w",encoding='UTF-8',newline ="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["category","overheads"])
+        file.close()
 
+for file in file_path("*Overheads*.csv"):
+        emptylist = []
 with file_path.open(mode="r",encoding='UTF-8',newline ="") as file:
         text = file.read()
+        Category = (re.search(pattern="Block.+", string=text))
+        overheads = (re.findall(pattern="[0-9].+", string=text))
+
+
 category = "Salary expense","Interest expense,Marketing expense","Overflow expense-retail","Overflow expense-warehouse","Penalty expense","Depreciation expense","Maintenanec expense","Shipping expense","Huamn resource expense"
+
 overheads = (re.findall(pattern="[0-9].+", string=text))
 for i in range (1):
-        emptylist.append(category)
-        emptylist.extend(overheads)
+        emptylist(max(overheads))
+        
 print(emptylist)
         
 
