9ed46aeae628c29b755c64680d8845269aa054f4
Jeff <S10247657@connect.np.edu.sg>
2022-08-01 17:46:26 +0800

    edit

--- overheads.py
+++ overheads.py
@@ -6,15 +6,15 @@ from platform import java_ver
 import re
 from unicodedata import category
 
+
 file_path = Path.cwd()/"CSV_reports"
 file_path = Path.cwd()/"CSV_reports"/"Overheads.csv"
-emptylist =[]
-        
+
+
 with file_path.open(mode="r",encoding='UTF-8',newline ="") as file:
-        text = file.read()
-        Category = (re.search(pattern="Block.+", string=text))
+        reader=csv.reader(file)
+        text = file.read() 
+        category = re.findall(pattern="[A-Z].+", string=text)
         overheads = (re.findall(pattern="[0-9].+", string=text))
-print(category)
 
-
-
+        print(category[0])
