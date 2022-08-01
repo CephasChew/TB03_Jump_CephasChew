d747b519aa9c1b3ae041a78e07cd4dd25c065093
tlxr05 <s10243109@connect.np.edu.sg>
2022-08-01 16:00:02 +0800

    changes

---
+++ coh.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import csv
+file1 = open(Path.cwd()/"CSV_reports"/"Cash on Hand.csv")
+writer = csv.writer(file1)
+for file in Path(Path.cwd()/"CSV_reports"/"Cash on Hand.csv")
+    
+
