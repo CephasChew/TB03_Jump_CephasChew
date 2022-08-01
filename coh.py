cd4dc28e769f18bc79aaf612ccb2dc6410ae3819
tlxr05 <s10243109@connect.np.edu.sg>
2022-08-01 16:33:41 +0800

    new

--- coh.py
+++ coh.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 import csv
+file_1 = Path.cwd()/"CSV_reports"/"Cash on Hand.csv"
 file1 = open(Path.cwd()/"CSV_reports"/"Cash on Hand.csv")
 writer = csv.writer(file1)
-for file in Path(Path.cwd()/"CSV_reports"/"Cash on Hand.csv")
+
+for file in file_1:
+    f = open(file, "r")
+    coh = f.read()
+
     
 
