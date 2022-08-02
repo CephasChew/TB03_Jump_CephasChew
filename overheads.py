831763db5368db44cb096e3ea6cbf281a5773b4b
Jeff <S10247657@connect.np.edu.sg>
2022-08-02 16:17:34 +0800

    done

--- overheads.py
+++ overheads.py
@@ -1,16 +1,10 @@
 from pathlib import Path
 import csv
 
-
-
 empty=[]
 
 file_path = Path.cwd()/"CSV_reports"
 overheads = Path.cwd()/"CSV_reports"/"Overheads.csv"
-overheads.touch()
-overheads_list = [["Salary Expense",24.18], ["Interest Expense",2.25], ["Marketing Expense",5.59],["Rental Expense",20.28],["Overflow Expense - Retail",3.88],["Overflow Expense - Warehouse",0.11],["Penalty Expense",7.66],["Depreciation Expense",16.72],["Maintenance Expense",7.94],["Shipping Expense",0.3],["Human Resource Expense",11.09]]
-        
-
 
 with overheads.open(mode="r", encoding = "UTF-8") as file:
         reader = csv.reader(file)
