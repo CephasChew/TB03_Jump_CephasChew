2c19948de04fdaa8818f15aab1e395251444554a
Jeff <S10247657@connect.np.edu.sg>
2022-08-03 14:07:34 +0800

    done

--- overheads.py
+++ overheads.py
@@ -1,24 +1,28 @@
 from pathlib import Path
 import csv
 
+
 empty=[]
+category=[]
 
 file_path = Path.cwd()/"CSV_reports"
 overheads = Path.cwd()/"CSV_reports"/"Overheads.csv"
 
+#creates 2 empty list to store category and overheads into a list
 with overheads.open(mode="r", encoding = "UTF-8") as file:
         reader = csv.reader(file)
         next(reader)
         for line in reader:
                 empty.append(line[1])
-                
+                category.append(line[0])
+
+#create another empty list change it into float
 new=[]
 for info in empty:
         info= float(info)
         new.append(info)
 
-print(new)
-
+#group it by using the define function and through using loop to find the latest value
 def highest():
         largestvalue=-1
 
@@ -26,6 +30,6 @@ def highest():
                 if each > largestvalue:
                         largestvalue = each
 
-        print("[HIGHEST OVERHEADS] Salary Expense:", largestvalue)
+        print("[HIGHEST OVERHEADS]",category[0],":", largestvalue)
 highest()
                 
