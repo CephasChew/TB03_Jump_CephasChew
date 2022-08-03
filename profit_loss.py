ddf58a5bfc085d035148f72ae5f379f45c9cb0d0
ZyNeo <s10239701@connect.np.edu.sg>
2022-08-03 14:28:39 +0800

    commit

--- profit_loss.py
+++ profit_loss.py
@@ -20,7 +20,7 @@ with file_path.open(mode='r', encoding= 'UTF-8', newline= '') as file:
         for info in line:
             #to append item at the end of the list
             empty_list.append(info)
-print(empty_list)
+# print(empty_list)
 
 #creating a new list
 new_list=[]
@@ -29,7 +29,7 @@ for info in empty_list:
     info = int(info)
     #change the numbers into integers with int()
     new_list.append(info)
-print(new_list)
+# print(new_list)
 #count the number of elements in the new list with len()
 no=(len(new_list))
 #creating a new list for the deficit
@@ -39,13 +39,13 @@ for num in range (no):
     if num%5==4:
         #comparing values and setting condition
         deficit_list.append(new_list[num])
-print(deficit_list)
-print(len(deficit_list))
+# print(deficit_list)
+# print(len(deficit_list))
 
 for i in range(len(deficit_list)-1):
     deficit_list[i] = deficit_list[i] - deficit_list[i+1]
 deficit_list=(deficit_list[0:(len(deficit_list)-1)])
-print(deficit_list)
+# print(deficit_list)
 
 #creating new lists for positive deficit values and deficit days
 positive_deficit_values=[]
@@ -58,8 +58,8 @@ for values in deficit_list:
         index= deficit_list.index(values)
         new_list_index=((index+4)*2)
         deficit_days.append(new_list[new_list_index])
-print(positive_deficit_values)
-print(deficit_days)
+# print(positive_deficit_values)
+# print(deficit_days)
 
 for i in range(len(deficit_days)):
     print(f"[PROFIT DEFICIT] DAY: {deficit_days[i]}, AMOUNT: SGD{positive_deficit_values[i]} ")
