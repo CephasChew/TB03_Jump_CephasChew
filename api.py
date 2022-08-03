066f8d4005d45c90d900ef859aad9374b67bed68
CephasChew <s10242109@connect.np.edu.sg>
2022-08-03 14:23:04 +0800

    Change to API

--- api.py
+++ api.py
@@ -1,23 +1,19 @@
 import json
 import requests
 
+#Assign the free api key to a variable to add in th website later
 api_key= "DAL5U3E14Y45E3DK"
-url=f"https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
-
+#The URL link with the function, from_currency and to_currency correctly assigned
+url=f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+#This line allows me to access the data from alphavantage
 response=requests.get(url)
+#Use . json() to retrieve data and stored as JSON object from the API.
+#Python will convert the JSON object as dictionary automatically.
 data=response.json()
-# print(json.dumps(data, indent=4))
-# print(len(data["Time Series FX (Weekly)"]))
-open_list=[]
-for date in data["Time Series FX (Weekly)"]:
-    open_list.append(data["Time Series FX (Weekly)"][date]["1. open"])
-    open_list.append(data["Time Series FX (Weekly)"][date]["2. high"])
-    open_list.append(data["Time Series FX (Weekly)"][date]["3. low"])
-    open_list.append(data["Time Series FX (Weekly)"][date]["4. close"])
-print(len(open_list))
-empty_list=[]
-for number in open_list:
-    number=float(number)
-    empty_list.append(number)
-average_USD_to_SGD=(sum(empty_list)/len(open_list))
-print(average_USD_to_SGD)
+#Lastly, I access the current exchange rate from USD to SGD. 
+#If you look at the nested dictionary, it starts with "Realtime Currency Exchange Rate", thus I access that first.
+#Next I access the current exchange rate hidden behind "5. Exchange Rate"
+#Then I just print out the exchange rate 
+print(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+
+
