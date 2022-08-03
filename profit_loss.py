f2b8d62e9752ce6c9354dc64861863050f370cb2
ZyNeo <s10239701@connect.np.edu.sg>
2022-08-03 13:43:35 +0800

    e

--- profit_loss.py
+++ profit_loss.py
@@ -1,32 +1,57 @@
-from ast import Num
+from ast import pattern
+from dataclasses import replace
+from operator import index
 from pathlib import Path
 import csv
-empty_list = []
-file_path = Path.cwd()/"CSV_reports"/"Profits and Loss.csv"
-with file_path.open(mode='r',encoding='UTF-8',newline='') as file:
+from posixpath import sep
+import re
+
+empty_list=[]
+file_path=Path.cwd()/"CSV_reports"/"Profits and Loss.csv"
+#Current working directory for the folder
+
+
+with file_path.open(mode='r', encoding= 'UTF-8', newline= '') as file:
     reader=csv.reader(file)
     next(reader)
     for line in reader:
-        print(line)
-        for value in line:
-            empty_list.append(int(value))
+        for info in line:
+            empty_list.append(info)
 print(empty_list)
 
-1
-if empty_list[4]>empty_list[9]:
-    net_profit = abs(empty_list[9]-empty_list[4])
-    print(f'[PROFIT DEFICIT] DAY: {empty_list[5]}, AMOUNT:SGD{net_profit}')
-elif empty_list[9]>empty_list[14]:
-    net_profit = abs(empty_list[14]-empty_list[9])
-    print(f'[PROFIT DEFICIT] DAY: {empty_list[10]}, AMOUNT:SGD{net_profit}')
-elif empty_list[14]>empty_list[19]:
-    net_profit = abs(empty_list[19]-empty_list[14])
-    print(f'[PROFIT DEFICIT] DAY: {empty_list[15]}, AMOUNT:SGD{net_profit}')
-elif empty_list[19]>empty_list[24]:
-    net_profit = abs(empty_list[24]-empty_list[19])
-    print(f'[PROFIT DEFICIT] DAY: {empty_list[20]}, AMOUNT:SGD{net_profit}')
-elif empty_list[24]>empty_list[29]:
-    net_profit = abs(empty_list[29]-empty_list[24])
-    print(f'[PROFIT DEFICIT] DAY: {empty_list[25]}, AMOUNT:SGD{net_profit}')
-else:
-    print("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+
+new_list=[]
+for info in empty_list:
+    info = int(info)
+    new_list.append(info)
+print(new_list)
+
+no=(len(new_list))
+
+deficit_list=[]
+
+for num in range (no):
+    if num%5==4:
+        deficit_list.append(new_list[num])
+print(deficit_list)
+print(len(deficit_list))
+
+for i in range(len(deficit_list)-1):
+    deficit_list[i] = deficit_list[i] - deficit_list[i+1]
+deficit_list=(deficit_list[0:(len(deficit_list)-1)])
+print(deficit_list)
+
+
+positive_deficit_values=[]
+deficit_days=[]
+for values in deficit_list:
+    if values>0:
+        positive_deficit_values.append(values)
+        index= deficit_list.index(values)
+        new_list_index=((index+4)*2)
+        deficit_days.append(new_list[new_list_index])
+print(positive_deficit_values)
+print(deficit_days)
+
+for i in range(len(deficit_days)):
+    print(f"[PROFIT DEFICIT] DAY: {deficit_days[i]}, AMOUNT: SGD{positive_deficit_values[i]} ")
