e524ffc45fef415f4a033f2b6a66b7536b48db32
farisyaaa <S10242602@connect.np.edu.sg>
2022-08-03 13:58:24 +0800

    1

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from pathlib import Path
 import api,coh,overheads,profit_loss
 
+# create a file path for summary_report.txt
+file_path = Path.cwd()/"CSV_reports"/"summary_report.txt"
+#create a text file "summary_report" in the file path
+file_path.touch()
+# check to see if summary_report.txt was made successfully in the file path
+print(file_path.exists())
+
+
+def overhead_function(forex):
+    
+
+
+
+
+
 def main():
     forex=api.api_function()
     overheads.overhead_function(forex)
     coh.coh_function(forex)
     profit_loss.profitloss_function(forex)
-
-
-file_path = Path.cwd()/"CSV_reports"/"summary_report.txt"
-file_path.touch()
-print(file_path.exists())
+main()
+    
