2400aa1fdffac4c9ead0ce215426bac7bc6c29ab
CephasChew <s10242109@connect.np.edu.sg>
2022-08-04 15:33:35 +0800

    change to api

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ import json,requests
 from pathlib import Path
 
 def api_function():
-   
+      
    #Assign the free api key to a variable to add in the website later
    api_key= "DAL5U3E14Y45E3DK"
    #The URL link with the function, from_currency and to_currency correctly assigned
@@ -16,9 +16,10 @@ def api_function():
    #If you look at the nested dictionary, it starts with "Realtime Currency Exchange Rate", thus I access that first.
    #Next I access the current exchange rate hidden behind "5. Exchange Rate"
    #Then I just print out the exchange rate 
-   xchange_rate=(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
-   #Making the value a float so it can be calculated later
+   xchange_rate=(data['Realtime Currency Exchange Rate']["5. Exchange Rate"])
+   # #Making the value a float so it can be calculated later
    xchange_rate=float(xchange_rate)
+   print(xchange_rate)
 
    summary_path = Path.cwd()/"CSV_reports"/"summary_report.txt"
    with summary_path.open(mode="a", encoding="UTF-8", newline="") as file:
